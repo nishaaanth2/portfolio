3e0ee67b10f9033977ac1ccdb3f020712f5dd9a7
nishaaanth2 <itismenishaanth@gmail.com>
2023-12-22 04:53:15 +0530

    finished

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import yosoku from "@/public/yosoku.png";
+import pixnot from "@/public/pixnot.png";
+import diostec from "@/public/diostec.png";
 
 export const links = [
   {
@@ -35,52 +35,60 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Computer Science Engineering",
+    location: "Jeppiaar Engineering College",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "With a four-year bachelor's degree, numerous hackathon victories, and event leadership. Organized coding symposiums, conducted workshops, and managed a coding club.",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "2017-2021",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "Back-End Developer",
+    location: "AIOTY Labs, Chennai",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "Proficient in Flask, MQTT, and implementing machine learning models (.mlmodel).",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "Oct 2020 - Sep 2021",
   },
   {
     title: "Full-Stack Developer",
-    location: "Houston, TX",
+    location: "Guardian Link, Chennai",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      "Led the architecture and team NFT Analytics development using ReactJS, ExpressJS, Python, and AWS Lambda.",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "2021 Oct - 2022 Jul",
+  },
+  {
+    title: "Full-Stack Developer",
+    location: "Orgware, Chennai",
+    description:
+      "Expertise in Next.js, ReactJS, ExpressJS, AWS and various databases. Managed project structure and ensured timely delivery.",
+    icon: React.createElement(FaReact),
+    date: "Aug 2022 - Nov 2023",
   },
 ] as const;
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Pixnot",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+      "AI image, code, video, music generator and AI chat.",
+    tags: [ "Next.js", "shadCN", "Tailwind", "Prisma", "cockroachDB", "clerkAuth"],
+    imageUrl: pixnot,
   },
   {
-    title: "rmtDev",
+    title: "Yosoku",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+      "Harness predictive analytics to anticipate future stock values and deliver tailored user recommendations.",
+    tags: ["React", "TypeScript", "MongoDB", "Tailwind", "AWS"],
+    imageUrl: yosoku,
   },
   {
-    title: "Word Analytics",
+    title: "Diostec",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+      "Job Placement static website",
+    tags: [ "Next.js", "GSAP", "Tailwind", "FramerMotion"],
+    imageUrl: diostec,
   },
 ] as const;
 
@@ -96,12 +104,12 @@ export const skillsData = [
   "Tailwind",
   "Prisma",
   "MongoDB",
-  "Redux",
-  "GraphQL",
-  "Apollo",
-  "Express",
   "PostgreSQL",
+  "AWS",
+  "Web3",
+  "shadCN",
+  "Express",
   "Python",
-  "Django",
+  "GSAP",
   "Framer Motion",
 ] as const;
