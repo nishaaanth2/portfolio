d04056268134c49f9cb0e70182c8e0ef0f66e8d4
nishaaanth2 <itismenishaanth@gmail.com>
2024-10-25 01:53:39 +0530

    scripyt

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,8 +77,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <Head>
-
       <Script async strategy="afterInteractive" id="analytics">
         {`
           (function(w,d,s,l,i){
@@ -92,7 +90,6 @@ export default function RootLayout({
           })(window,document,'script','dataLayer','GTM-K7J7QRKF');
         `}
       </Script>
-      </Head>
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
